b719a8c670b8da677ac68968115a7a4904d1bf5c
hendrik <24483010+kwagga333@users.noreply.github.com>
2024-05-19 22:57:07 +0200

    Add func userInput and some else if
    else if for eval for rock paper and scissors

--- main.js
+++ main.js
@@ -1,11 +1,55 @@
-console.log("Hello World");
 
 
-let arr = ["Rock", "Paper", "Scissors"];
-
+let arr = ["ROCK", "PAPER", "SCISSORS"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
 
-console.log(arr[(Math.floor(Math.random() * arr.length))]);
+return(arr[(Math.floor(Math.random() * arr.length))]);
 
 };
+
+
+
+function getHumanChoice(){
+
+    let userPrompt = prompt("Type Rock, Paper, Scissors");
+    let userInput = userPrompt.toUpperCase();
+
+    return(userInput);
+
+};
+
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice === computerChoice || computerChoice === humanChoice) {
+        console.log("It's a Draw");
+    }
+    else if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+        console.log("You Win! Rock beats Scissors");
+    }
+    else if(humanChoice === "ROCK" && computerChoice === "PAPER"){
+        console.log("You Lose! Paper beats Rock" )
+    }
+    else if(computerChoice === "ROCK" && humanChoice === "PAPER"){
+        console.log("You Win! Paper beats Rock")
+    }
+    else if(computerChoice === "ROCK" && humanChoice === "SCISSOR"){
+        console.log("You Lose! Rock beats Paper");
+    }
+    else if(humanChoice === "SCISSOR" && computerChoice === "PAPER"){
+        console.log("You Win! Scissor beat Paper");
+    }
+    else if(humanChoice === "SCISSOR" && computerChoice === "ROCK"){
+        console.log("You Lose! Rock beats Scissors");
+    }
+    else if(humanChoice === "PAPER" && computerChoice ==="ROCK"){
+        
+    }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
